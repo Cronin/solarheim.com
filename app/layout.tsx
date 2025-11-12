9a7d74c193fd9a5ec017b97e646128948f6b1d72
cronin <58665249+Cronin@users.noreply.github.com>
2025-11-12 18:21:46 +0800

    feat: install Microsoft Clarity tracking (project u1ur4kb2kq)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -145,7 +146,21 @@ export default function RootLayout({
 })
           }}
         />
-      </body>
+            {/* Microsoft Clarity */}
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u1ur4kb2kq");
+          `,
+        }}
+      />
+</body>
     </html>
   );
 }
